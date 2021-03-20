08fe348df956607b1b6b19aeb4ff8463d54d40c7
Saksham <sakshamyadavpune@gmail.com>
2021-03-20 19:38:32 +0530

    first commit

---
+++ controllers/image.js
@@ -0,0 +1,29 @@
+const { json } = require('body-parser');
+const clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: 'ccb30964d3b342d2875ea95b459b29dc'
+  });
+
+const handleApiCall=(req,res)=>{
+    app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data=>{
+        res.json(data);
+    })
+    .catch(err=>res.status(400).json('Api Error'));
+}
+
+const handleImage=(req,res,db)=>{
+    const { id } = req.body;
+    db('users').where({ id })
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json('Unable to get Entries'))
+}
+module.exports={
+    handleImage,
+    handleApiCall
+}
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const body_parser = require('body-parser');
+const cors = require('cors');
+const knex = require('knex');
+const bcrypt = require('bcrypt-nodejs');
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'root',
+        database: 'smartbrain'
+
+    }
+});
+
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json(database.users)
+})
+
+app.post('/signin', (req,res)=>{signin.handleSignIn(req,res,db,bcrypt)})
+
+app.post('/register', (req,res)=>{register.handleRegister(req,res,db,bcrypt)})
+
+app.get('/profile/:id', (req, res)=>{profile.handleProfile(req,res)})
+
+app.put('/image', (req, res) => {image.handleImage(req,res,db)})
+
+app.post('/imageurl', (req,res)=>{image.handleApiCall(req,res)})
+
+app.listen(3001, () => {
+    console.log("Hellooo Port 3001!")
+})
